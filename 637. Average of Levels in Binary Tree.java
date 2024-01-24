0367b447a94539fb3feb68e438734d43361b6453
Vivek kumar <122138413+Vivekgupta1201@users.noreply.github.com>
2024-01-24 13:30:17 +0530

    637. Average of Levels in Binary Tree.java

---
+++ 637. Average of Levels in Binary Tree.java
@@ -0,0 +1,60 @@
+// Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [3.00000,14.50000,11.00000]
+// Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+// Hence return [3, 14.5, 11].
+// Example 2:
+
+
+// Input: root = [3,9,20,15,7]
+// Output: [3.00000,14.50000,11.00000]
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -231 <= Node.val <= 231 - 1
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans= new ArrayList<>();
+        Queue<TreeNode> level=new LinkedList<>();
+        level.add(root);
+        while(!level.isEmpty()){
+              Double sum=0.0;
+              int n=level.size();
+            for(int i=0;i<n;i++){
+                TreeNode nod=level.peek();
+
+              if(nod.left != null) level.add(nod.left);
+                if(nod.right != null)level.add(nod.right);
+                sum+=level.poll().val;
+
+            }
+            ans.add(sum/n);
+
+        }
+       return ans;
+    }
+}
